7d4fea6533efb08efff32b21f996cb0ec05ccd6d
jesmarver <jesusperike@gmail.com>
2025-09-24 11:31:18 +0200

    test(CronService): Test suite for cronService added

--- src/presentation/cron/cron.service.ts
+++ src/presentation/cron/cron.service.ts
@@ -3,10 +3,10 @@ import { CronJob } from "cron";
 type CronTime = string | Date;
 type OnTick = () => void;
 
-export class CronService{
+export class CronService {
 
-    public static createJob(cronTime : CronTime, onTick: OnTick): CronJob{
-                const job = new CronJob(
+    public static createJob(cronTime: CronTime, onTick: OnTick): CronJob {
+        const job = new CronJob(
             cronTime, // cronTime
             onTick,
         );
